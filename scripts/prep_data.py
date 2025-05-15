9facc716537f05538c48afd199174be942347040
kassyray <Kassy.Raymond@wdgpublichealth.ca>
2025-05-15 18:19:39 +0000

    Adding checks for system arguments

--- scripts/prep_data.py
+++ scripts/prep_data.py
@@ -2,9 +2,31 @@ import pandas as pd
 import sys
 import re
 from datetime import date
-from utils import calculate_age
+from utils import calculate_age, over_16_check
 import yaml
 import json
+from collections import defaultdict
+import os
+
+# Check to see if correct number of system arguments   
+if len(sys.argv) != 4:
+    print("Usage: python prep_data.py <vaccination_file> <config_file> <disease_map_file>")
+    sys.exit(1)
+
+# Check to see if the vaccination file exists
+if not os.path.isfile(sys.argv[1]):
+    print(f"Vaccination file {sys.argv[1]} does not exist.")
+    sys.exit(1)
+
+# Check to see if the config file exists
+if not os.path.isfile(sys.argv[2]):
+    print(f"Config file {sys.argv[2]} does not exist.")
+    sys.exit(1)
+
+# Check to see if the disease map file exists
+if not os.path.isfile(sys.argv[3]):
+    print(f"Disease map file {sys.argv[3]} does not exist.")
+    sys.exit(1)
 
 # Read vaccination file
 path_vax = sys.argv[1]
@@ -23,12 +45,25 @@ with open (path_disease_map, 'r') as f:
 # Take info out of config file
 expected_columns = data['expected_columns']
 ignore_agents = data['ignore_agents']
+delivery_date = data['delivery_date']
 
 # Conduct checks of the data using yaml file...
 # Check to see if the expected columns in the df match what is in the yaml
 if expected_columns != list(df.columns):
     print(f"Column mismatch. \nExpected {expected_columns}.\nFound: {list(df.columns)}")
     sys.exit(1)
+
+print(expected_columns)
+
+# Create default dictionary for restructuring data
+notices = defaultdict(lambda: {
+    "name": "",
+    "date_of_birth": "",
+    "age": "",
+    "over_16": "",
+    "vaccines_due": "",
+    "recieved": []
+})
 
 # Rename columns (remove spaces) for further processing...
 df.columns = df.columns.str.replace(' ', '_')
@@ -44,11 +79,26 @@ for index, row in df.iterrows():
         mapped = disease_map.get(vax_to_compare, vax_to_compare)
         vaccines_due_updated.append(mapped)
     
-    # Replace col based on mapped values 
-    # First convert it into a string
-    # FIXME - should there be a comma after the last disease? 
+    # # Replace col based on mapped values 
+    # # First convert it into a string
+    # # FIXME - should there be a comma after the last disease? 
     vaccines_due_str = ', '.join(str(e) for e in vaccines_due_updated)
     df.at[index, 'Vaccines_Due'] = vaccines_due_str
+
+    # Check if the client is over 16 years old
+    over_16 = over_16_check(row.Date_of_Birth, delivery_date)
+
+    client_id = row.Client_ID
+
+    notices[client_id]["name"] = row.First_Name + " " + row.Last_Name
+    notices[client_id]["date_of_birth"] = row.Date_of_Birth
+    notices[client_id]["address"] = row.Street_Address
+    notices[client_id]["city"] = row.City
+    notices[client_id]["postal_code"] = row.Postal_Code
+    notices[client_id]["province"] = row.Province
+    notices[client_id]["over_16"] = over_16
+    notices[client_id]["age"] = calculate_age(row.Date_of_Birth, "Apr 8, 2025")
+    notices[client_id]["vaccines_due"] = vaccines_due_str
 
     matches = re.findall(r'\w{3} \d{1,2}, \d{4} - [^,]+', row.Received_Agents)
     
@@ -67,4 +117,4 @@ for index, row in df.iterrows():
                 'age':calculate_age(row.Date_of_Birth, date_str)
             })
 
-#print(structured_entries)
+print(notices)
