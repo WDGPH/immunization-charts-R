8a3a6a0932e55132b6651025fac67181af3ef816
kassray <kassy.raymond@wdgpublichealth.ca>
2025-06-06 19:51:27 +0000

    fix for batch files

--- scripts/run_data_pipeline.sh
+++ scripts/run_data_pipeline.sh
@@ -20,6 +20,7 @@ echo ""
 if [ ! -f "${INDIR}/vaccine_reference.xlsx" ]
 then
     echo "ERROR! File ${INDIR}/vaccine_reference.xlsx does not exist. Check your system arguments to ensure you are specifying the correct input directory. Check to ensure that the vaccine reference sheet exists and is in the correct directory."
+    exit 1
 fi
 
 echo ""
@@ -84,15 +85,23 @@ NR==1 {header = $0; next}
 
 echo ""
 echo ""
+echo "Generating a list of schools that appear in the file for further processing"
+echo ""
+echo ""
+
+awk -F';' '{print $2}' ${INDIR}/anonymized_data_sample.csv | sort | uniq > "${OUTDIR}/schools_list.txt"
+
+echo ""
+echo ""
 echo "Separating files by language"
 echo ""
 echo ""
 
-mkdir -p "${OUTDIR}/by_language_school"
+mkdir -p "${OUTDIR}/by_language"
 
 echo ""
 echo ""
-echo "Created directory ${OUTDIR}/by_language_school"
+echo "Created directory ${OUTDIR}/by_language"
 echo ""
 echo ""
 
@@ -114,7 +123,7 @@ do
         }
         NR==1 {header = $0; next}
         $1 == lang {
-        file = "'${OUTDIR}'/by_language_school/" lang "_" basefile ".csv";
+        file = "'${OUTDIR}'/by_language/" lang "_" basefile ".csv";
         if (!(file in seen)) {
             print header > file;
             seen[file]
@@ -124,7 +133,6 @@ do
     done
 done
 
-
 echo ""
 echo ""
 echo "Pulling batch size from yaml file"
@@ -132,116 +140,139 @@ echo ""
 echo ""
 
 batch_size=$(grep '^batch_size:' ../config/parameters.yaml | awk '{print $2}')
+echo ""
+echo ""
+echo "Separating csv files according to batch size: $batch_size"
+echo ""
+echo ""
 
 echo ""
 echo ""
-echo "Separating csv files according to batch size: ${batch_size)}"
+echo "Storing files in batched directory"
 echo ""
 echo ""
 
-for file in `ls ${OUTDIR}/by_language_school/`
+mkdir -p "${OUTDIR}/batched"
+
+# For each CSV in the input directory
+for file in "${OUTDIR}/by_language/"*.csv
 do
-    filename=$(echo "$file" | cut -f 1 -d '.')
-    split -l ${batch_size} --filter='cat > ${FILE}.csv' ${OUTDIR}/by_language_school/${file} ${OUTDIR}/by_language_school/${filename}_
+    filename=$(basename "$file" .csv)
+    header=$(head -n 1 "$file")
+
+    awk -v header="$header" -v batch_size="$batch_size" -v outdir="${OUTDIR}/batched" -v base="$filename" '
+    NR > 1 {
+        file_idx = int((NR-2)/batch_size) + 1
+        file = sprintf("%s/%s_%02d.csv", outdir, base, file_idx)
+        if (!(file in seen)) {
+            print header > file
+            seen[file] = 1
+        }
+        print >> file
+    }
+    ' "$file"
+
 done
 
-echo ""
-echo ""
-echo "Processing and transforming ENGLISH data from csv to structure json..."
-echo ""
-echo ""
-
-mkdir -p "${OUTDIR}/english_json"
-echo "" 
-echo ""
-echo "Created directory ${OUTDIR}/english_json"
-echo ""
-echo ""
-
-for i in `ls ${OUTDIR}/by_language_school/*_a*`
-do
-    if [[ $i == *"English"* ]]; then
-        echo "Processing: $i"
-        python prep_data.py "${OUTDIR}/by_language_school/${i}" "../config/parameters.yaml" "../config/disease_map.json" "../input/vaccine_reference.json" "${OUTDIR}/english_json"
-    fi
-done
-
-echo ""
-echo ""
-echo "Processing and transforming FRENCH data from excel to structure json..."
-echo ""
-echo ""
-
-mkdir -p "${OUTDIR}/french_json"
-echo ""
-echo ""
-echo "Created directory ${OUTDIR}/french_json"
-echo ""
-echo ""
-
-for i in `ls ${OUTDIR}/by_language_school/`
-do
-    if [[ $i == *"French"* ]]; then
-        echo "Processing: $i"
-        python prep_data.py "${OUTDIR}/by_language_school/${i}" "../config/parameters.yaml" "../config/disease_map.json" "../input/vaccine_reference.json" "${OUTDIR}/french_json"
-    fi
-done
-
-echo ""
-echo ""
-echo "Data processing complete. The json files are located in the ${OUTDIR}/english_json and ${OUTDIR}/french_json directories."
-echo ""
-echo ""
-
-END_PREPROCESSING=$(date +%s)
-DIFF=$(( $END_PREPROCESSING - $START_PREPROCESSING ))
-echo "Data preprocessing complete. Total time taken: $DIFF seconds"
-
-echo ""
-echo ""
-echo "Now generating the immunization notice templates..."
-echo ""
-echo ""
-
-echo ""
-echo ""
-echo "Generating immunization notice templates for English data..."
-echo ""
-echo ""
-
 # echo ""
 # echo ""
-# echo "Getting list of json files in ${OUTDIR}/english_json"
+# echo "Processing and transforming ENGLISH data from csv to structure json..."
 # echo ""
 # echo ""
 
-# START_TEMPLATE_GENERATION=$(date +%s)
+# mkdir -p "${OUTDIR}/english_json"
+# echo "" 
+# echo ""
+# echo "Created directory ${OUTDIR}/english_json"
+# echo ""
+# echo ""
 
-# for jsonfile in ${OUTDIR}/english_json/*.json
+# for i in `ls ${OUTDIR}/by_language_school/*_a*.csv`
 # do
-#     if [ -f "$jsonfile" ]; then
-#         filename=$(basename "$jsonfile" .json)
-#         echo "Generating template for $filename"
-#         ./generate_template.sh ${OUTDIR}/english_json "$filename" "../../config/parameters.yaml" "../../templates/assets/logo.svg"
-#     else
-#         echo "No JSON files found in ${OUTDIR}/english_json."
+#     if [[ $i == *"English"* ]]; then
+#         echo "Processing: $i"
+#     python prep_data.py "$i" "../config/parameters.yaml" "../config/disease_map.json" "../input/vaccine_reference.json" "${OUTDIR}/english_json"
 #     fi
 # done
 
-# END_TEMPLATE_GENERATION=$(date +%s)
-# DIFF=$(( $END_TEMPLATE_GENERATION - $START_TEMPLATE_GENERATION ))
-# echo "Template generation complete for English data. Total time taken: $DIFF seconds"
+# echo ""
+# echo ""
+# echo "Processing and transforming FRENCH data from excel to structure json..."
+# echo ""
+# echo ""
 
-# START_TEMPLATE_COMPILATION=$(date +%s)
+# mkdir -p "${OUTDIR}/french_json"
+# echo ""
+# echo ""
+# echo "Created directory ${OUTDIR}/french_json"
+# echo ""
+# echo ""
 
-# typst compile --font-path ../templates/assets/ --root ../ ../output/english_json/English_Maple_Syrup_High_immunization_notice.typ
+# # for i in `ls ${OUTDIR}/by_language_school/`
+# # do
+# #     if [[ $i == *"French"* ]]; then
+# #         echo "Processing: $i"
+# #         python prep_data.py "${OUTDIR}/by_language_school/${i}" "../config/parameters.yaml" "../config/disease_map.json" "../input/vaccine_reference.json" "${OUTDIR}/french_json"
+# #     fi
+# # done
 
-# END_TEMPLATE_GENERATION=$(date +%s)
-# DIFF=$(( $END_TEMPLATE_GENERATION - $START_TEMPLATE_COMPILATION ))
-# echo "Template compilation complete for English data. Total time taken: $DIFF seconds"
+# echo ""
+# echo ""
+# echo "Data processing complete. The json files are located in the ${OUTDIR}/english_json and ${OUTDIR}/french_json directories."
+# echo ""
+# echo ""
 
-echo ""
-echo ""
-echo "Clean-up files"
-echo ""
-echo "" 
+# END_PREPROCESSING=$(date +%s)
+# DIFF=$(( $END_PREPROCESSING - $START_PREPROCESSING ))
+# echo "Data preprocessing complete. Total time taken: $DIFF seconds"
+
+# echo ""
+# echo ""
+# echo "Now generating the immunization notice templates..."
+# echo ""
+# echo ""
+
+# echo ""
+# echo ""
+# echo "Generating immunization notice templates for English data..."
+# echo ""
+# echo ""
+
+# # echo ""
+# # echo ""
+# # echo "Getting list of json files in ${OUTDIR}/english_json"
+# # echo ""
+# # echo ""
+
+# # START_TEMPLATE_GENERATION=$(date +%s)
+
+# # for jsonfile in ${OUTDIR}/english_json/*.json
+# # do
+# #     if [ -f "$jsonfile" ]; then
+# #         filename=$(basename "$jsonfile" .json)
+# #         echo "Generating template for $filename"
+# #         ./generate_template.sh ${OUTDIR}/english_json "$filename" "../../config/parameters.yaml" "../../templates/assets/logo.svg"
+# #     else
+# #         echo "No JSON files found in ${OUTDIR}/english_json."
+# #     fi
+# # done
+
+# # END_TEMPLATE_GENERATION=$(date +%s)
+# # DIFF=$(( $END_TEMPLATE_GENERATION - $START_TEMPLATE_GENERATION ))
+# # echo "Template generation complete for English data. Total time taken: $DIFF seconds"
+
+# # START_TEMPLATE_COMPILATION=$(date +%s)
+
+# # typst compile --font-path ../templates/assets/ --root ../ ../output/english_json/English_Maple_Syrup_High_immunization_notice.typ
+
+# # END_TEMPLATE_GENERATION=$(date +%s)
+# # DIFF=$(( $END_TEMPLATE_GENERATION - $START_TEMPLATE_COMPILATION ))
+# # echo "Template compilation complete for English data. Total time taken: $DIFF seconds"
+
+# echo ""
+# echo ""
+# echo "Clean-up files"
+# echo ""
+# echo "" 
+
+# rm -r ${OUTDIR}/by_language_school/
