ef8deca4d72301c3f5687b101cc55df43ed3cc63
kassray <kassy.raymond@wdgpublichealth.ca>
2025-06-18 17:49:14 +0000

    Pre-preocessing data quality. Still need to implement logging.

--- scripts/data_quality/pre_processing_utils.py
+++ scripts/data_quality/pre_processing_utils.py
@@ -1,6 +1,20 @@
 import pandas as pd
 import sys
 
+# Remove all special characters from column names
+def clean_column_names(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Cleans the column names of a DataFrame by removing all special characters.
+    Args:
+        df (pd.DataFrame): The DataFrame whose column names are to be cleaned.
+    Returns:
+        pd.DataFrame: The DataFrame with cleaned column names.
+    """
+    df.columns = df.columns.str.replace(r'[^a-zA-Z0-9_]', '', regex=True)
+    return df
+
+# Remove all special characters from each row
+
 # Deduplication of client records (check for uniqueness of each client)
 
 # Non-nullness
