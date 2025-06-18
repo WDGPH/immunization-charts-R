26b07633b89220bebd2b90222a4c2a05c83bbce5
kassray <kassy.raymond@wdgpublichealth.ca>
2025-06-18 17:37:56 +0000

    Adding in placeholder for the data quality framework, preprocessing utilities

---
+++ scripts/data_quality/pre_processing_utils.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import sys
+
+# Deduplication of client records (check for uniqueness of each client)
+
+# Non-nullness
+
+# Check to make sure that the age that is calculated is valid (no negative ages)
+
